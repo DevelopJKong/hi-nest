13e60eb21eeee14995e298a095a46a9038d88df8
DevelopJKong <ybaram40@gmail.com>
2022-01-17 11:35:55 +0900

    #3.1 Your first Unit Test 완료

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { CreateMovieDto } from './dto/create-movie.dto';
 import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 
 @Injectable()
 export class MoviesService {
@@ -12,26 +13,27 @@ export class MoviesService {
 
   getOne(id: number): Movie {
     const movie = this.movies.find(movie => movie.id === id);
-      if(!movie) {
-        throw new NotFoundException(`Movie with ID: ${id} Not Found`);
-      }
-      return movie;
-  } 
+    if (!movie) {
+      throw new NotFoundException(`Movie with ID ${id} not found.`);
+    }
+    return movie;
+  }
+
   deleteOne(id: number) {
     this.getOne(id);
     this.movies = this.movies.filter(movie => movie.id !== id);
-    return true;
   }
-  create(movieData:CreateMovieDto) {
-      this.movies.push({
-        id: this.movies.length + 1,
-        ...movieData,
-      });
+
+  create(movieData: CreateMovieDto) {
+    this.movies.push({
+      id: this.movies.length + 1,
+      ...movieData,
+    });
   }
-  update(id: number, updateData) {
+
+  update(id: number, updateData: UpdateMovieDto) {
     const movie = this.getOne(id);
     this.deleteOne(id);
-
-    this.movies.push({...movie,...updateData});
+    this.movies.push({ ...movie, ...updateData });
   }
 }
